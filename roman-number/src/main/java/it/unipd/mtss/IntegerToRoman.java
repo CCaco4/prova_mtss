6d6a6cd34b0d286a6f930f2947f2aaeb36fea5c7
CCaco4 <sanguinmarco07@gmail.com>
2026-05-09 00:01:04 +0200

    prova

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,7 +1,24 @@
+/////////////////////////////////////////////
+// FRANCESCO MARCON 2110990
+// MARCO SANGUIN 2103121
+////////////////////////////////////////////
+
 package it.unipd.mtss;
+
 public class IntegerToRoman {
-	public static String convert(int number){
-		// TODO
-		return null;
-	}
+    
+    public static String convert(int number) {
+        String[] romanLetters = { "X", "IX", "V", "IV", "I" };
+        int[] values = { 10, 9, 5, 4, 1 };
+        StringBuilder romanNumber = new StringBuilder();
+        
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                number -= values[i];
+                romanNumber.append(romanLetters[i]);
+            }
+        }
+        
+        return romanNumber.toString();
+    }
 }
